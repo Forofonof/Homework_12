0aec264e394ae39a55915c91547a2cadf9ab1bee
Forofonof <DanielResult1@outlook.com>
2022-04-08 17:45:01 +0300

    FiX_1.2

--- Homework_12/Program.cs
+++ Homework_12/Program.cs
@@ -9,19 +9,18 @@
         string userAge = " - ";
         string userMessage;
         double userBalanceEthereum = 0.35;
-        bool thisUserWantsOut = false;
+        bool isExit = false;
 
         Console.WriteLine($"Добро пожаловать! {userNickname}.");
         Console.WriteLine("Введите Info, чтобы узнать какие команды есть.");
         Console.WriteLine("Введите команду в консоль:");
 
-        while (thisUserWantsOut == false)
+        while (isExit == false)
         {
             userMessage = Console.ReadLine();
 
-            switch (userMessage)
+            switch (userMessage.ToLower())
             {
-                case "Info":
                 case "info":
                     Console.WriteLine("SetNickname - Поменять ник.");
                     Console.WriteLine("CheckBalance - Проверить баланс.");
@@ -30,37 +29,31 @@
                     Console.WriteLine("SetAge - Поменять возраст.");
                     Console.WriteLine("Exit - Выход из программы.");
                     break;
-                case "SetNickname":
                 case "setnickname":
                     Console.WriteLine("Введите новый ник:");
                     userNickname = Console.ReadLine();
                     Console.WriteLine($"Вы установили новый ник: '{userNickname}'");
                     break;
-                case "SetHobby":
                 case "sethobby":
                     Console.WriteLine("Введите свое хобби:");
                     userHobby = Console.ReadLine();
                     Console.WriteLine($"Вы поменяли свое хобби: '{userHobby}'");
                     break;
-                case "SetAge":
                 case "setage":
                     Console.WriteLine("Введите ваш возраст:");
                     userAge = Console.ReadLine();
                     Console.WriteLine($"Вы поменяли свой возраст: '{userAge} лет.'");
                     break;
-                case "CheckBalance":
                 case "checkbalance":
                     Console.WriteLine($"Ваш баланс: {userBalanceEthereum} ETH.");
                     break;
-                case "ProfileInfo":
                 case "profileinfo":
                     Console.WriteLine($"Ваш никнейм: {userNickname}");
                     Console.WriteLine($"Хобби: {userHobby}");
                     Console.WriteLine($"Возраст: {userAge} лет.");
                     break;
-                case "Exit":
                 case "exit":
-                    thisUserWantsOut = true;
+                    isExit = true;
                     break;
                 default:
                     Console.WriteLine("Команда введена неверно.");
